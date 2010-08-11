e27fe63de5682a9a60017fd54ebecc8cab482027
Leigh McCulloch <leigh@mcchouse.com>
2010-08-11 23:17:20 +1000

    Added some basic page/template code as well as the intended pages.

--- index.php
+++ index.php
@@ -1,10 +1,36 @@
 <?php
 
+define('GET_PAGE', 'page');
+
+/* valid pages */
+$PAGE_LIST=array(
+	'access_resources',
+	'manage_resource_ranges',
+	'manage_resource_types',
+	'manage_resource',
+	'manage_users'
+);
+
 /* includes */
 require_once('incs.php');
 
-/* setup the template */
+/* find page */
+$PAGE_DEFAULT_NAME='access_resources';
+$PAGE_NAME=$PAGE_DEFAULT_NAME;
+if(isset($_GET[GET_PAGE])) {
+	$PAGE_NAME=$_GET[GET_PAGE];
+	if(!in_array($PAGE_NAME, $PAGE_LIST)) {
+		$PAGE_NAME=$PAGE_DEFAULT_NAME;
+	}
+}
 
-/* switch to appropriate page */
+/* generate the page */
+ob_start();
+include('templates/default/'.$PAGE_NAME.'.php');
+$TEMPLATE_FIELD_BODY = ob_get_contents();
+ob_end_clean();
+
+/* display the page with the template */
+include('templates/default/page.php');
 
 ?>
---
+++ templates/default/page.php
@@ -0,0 +1,10 @@
+<HTML>
+<HEAD>
+<TITLE>the arms locker - Limited Resource Management</TITLE>
+</HEAD>
+<BODY>
+
+<?=$TEMPLATE_FIELD_BODY?>
+
+</BODY>
+</HTML>
