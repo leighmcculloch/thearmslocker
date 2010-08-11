f0e0b3eb0d034ff341b780fa6ad382770c3eb38a
Leigh McCulloch <leigh@mcchouse.com>
2010-08-11 23:40:43 +1000

    Added directory defines.

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+define('DIR_INCS', 'incs');
+define('DIR_PAGES', 'pages');
+define('DIR_TEMPLATES', 'templates');
+
 /* includes */
-require_once('incs/incs.php');
+require_once(DIR_INCS.'/incs.php');
 
 /* find page */
 if(isset($_GET[GET_PAGE])) {
@@ -13,11 +17,11 @@ if(isset($_GET[GET_PAGE])) {
 
 /* generate the page */
 ob_start();
-include('templates/default/'.$PAGE_NAME.'.php');
+include(DIR_PAGES.'/'.$PAGE_NAME.'.php');
 $TEMPLATE_FIELD_BODY = ob_get_contents();
 ob_end_clean();
 
 /* display the page with the template */
-include('templates/default/page.php');
+include(DIR_TEMPLATES.'/default/page.php');
 
 ?>
