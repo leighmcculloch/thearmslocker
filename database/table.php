3fe7de9906230bcb2494de64845c5aad7d2f765c
Leigh McCulloch <leigh@mcchouse.com>
2010-08-12 22:41:00 +1000

    Database work.

---
+++ database/table.php
@@ -0,0 +1,23 @@
+<?php
+
+class table
+{
+	private $m_tableName;
+	private $m_tableColumns=array();
+	private $m_tableRowCount=0;
+	
+	public table($tableName)
+	{
+		$m_tableName=$tableName;
+		
+		/* get a list of columns */
+		$m_tableColumns=$tableColumns;
+		
+		/*  */
+	}
+	
+	public 
+}
+
+?>
+
--- database/table_Users.php
+++ database/table_Users.php
@@ -2,6 +2,15 @@
 
 class table_Users
 {
+	private $m_id;
+	private $m_name;
+	
+	public table_Users()
+	{
+		
+	}
+	
+	public 
 }
 
 ?>
