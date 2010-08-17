47b735d052c06a649b601cf315a19ad703eeb628
Leigh McCulloch <leigh@mcchouse.com>
2010-08-17 22:18:33 +1000

    Created the manage_resource_types page.

--- web-app/pages/manage_resource_types.php
+++ web-app/pages/manage_resource_types.php
@@ -1,3 +1,23 @@
 <?php
 
+$table=new table_ResourceType;
+$types=$table->getRecords();
+
 ?>
+
+<table>
+	<tr>
+		<th>Resource Type ID</th>
+		<th colspan="2">Resource Type</th>
+	</tr>
+	<?php foreach($types as $type) : ?>
+	<tr>
+		<td><?php echo $type->id; ?></td>
+		<td class="edit_text" id="update(resourcetype[<?php echo $type->id; ?>].name)"><?php echo $type->name; ?></td>
+		<td class="delete" id="delete(resourcetype[<?php echo $type->id; ?>])">X</td>
+	</tr>
+	<?php endforeach; ?>
+	<tr>
+		<td colspan="3" class="create" id="create(resourcetype)">Create New Resource Type</td>
+	</tr>
+</table>
