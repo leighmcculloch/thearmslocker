3352a24674a26ba246ca49cb3eafe8787a9d4f0c
Leigh McCulloch <leigh@mcchouse.com>
2010-08-25 20:05:54 +1000

    Updated the copyright tag at the bottom.

--- web-app/incs/define.php
+++ web-app/incs/define.php
@@ -2,6 +2,10 @@
 
 define('LICENSE', 'MIT License');
 define('COPYRIGHT', 'Copyright &copy; 2010 Leigh McCulloch');
-define('ATTRIBUTE', '<a href="http://github.com/leighmcculloch/thearmslocker">The Arms Locker</a>, jQuery, jQuery UI');
+define('ATTRIBUTE', '<a href="http://github.com/leighmcculloch/thearmslocker">The Arms Locker</a>'
+				 .', <a href="http://jquery.com">jQuery</a>'
+				 .', <a href="http://jqueryui.com">jQuery UI</a>'
+				 .', <a href="http://www.appelsiini.net/projects/jeditable">Jeditable</a>'
+				 .', <a href="http://tablesorter.com/">tablesorter</a>');
 
 ?>
